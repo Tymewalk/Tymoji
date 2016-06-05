0ad6850d7d72e7d7b095245a6456cbedf4638e7c
Hampton <herohamp@users.noreply.github.com>
2016-06-04 22:34:56 -0400

    Update tymoji.user.js

--- tymoji.user.js
+++ tymoji.user.js
@@ -10,16 +10,6 @@
 // @version     0.1
 // @grant       metadata
 // ==/UserScript==
-function GetUrlValue(VarSearch){
-    var SearchString = window.location.search.substring(1);
-    var VariableArray = SearchString.split('&');
-    for(var i = 0; i < VariableArray.length; i++){
-        var KeyValuePair = VariableArray[i].split('=');
-        if(KeyValuePair[0] == VarSearch){
-            return KeyValuePair[1];
-        }
-    }
-}
 var posts = document.getElementsByClassName('post_body_html');
 var sigs = document.getElementsByClassName('postsignature');
 
